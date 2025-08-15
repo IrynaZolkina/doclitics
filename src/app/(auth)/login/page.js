7095eb032f7caf1f0f2d07ce6a965a7b3d5dceed
Iryna <iryna@MacBook-Pro-Iryna.local>
2025-08-15 11:30:33 +0300

    commit 17

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -1,18 +1,57 @@
+"use client";
+
 import "../../../app/globals.css";
 import styles from "../../css-modules/loginpage.module.css";
 
-// import { register } from "../../../actions/userservice";
-// import React, { useActionState } from "react";
-// import { useEffect } from "react";
-// import { useDispatch } from "react-redux";
-// import { addToast } from "../../../redux/store";
-// import { useSearchParams } from "next/navigation";
+import { register } from "../../../actions/userservice";
+import React, { useActionState } from "react";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { addToast } from "../../../redux/store";
+import { useSearchParams } from "next/navigation";
 import ClientToastHandler from "@/components-ui/ClientToastHandler";
 
-const LoginPage = () => {
+export default function LoginPage() {
+  const dispatch = useDispatch();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const registered = searchParams.get("registered");
+    const error = searchParams.get("error");
+
+    if (registered === "1") {
+      dispatch(
+        addToast({
+          id: Date.now(),
+          message: "Registration successful! Please login.",
+          type: "success",
+        })
+      );
+    }
+
+    if (error === "invalid_token") {
+      dispatch(
+        addToast({
+          id: Date.now(),
+          message: "Invalid activation link.",
+          type: "error",
+        })
+      );
+    }
+
+    if (error === "expired_token") {
+      dispatch(
+        addToast({
+          id: Date.now(),
+          message: "Activation link expired.",
+          type: "error",
+        })
+      );
+    }
+  }, [searchParams, dispatch]);
+
   return (
     <div className={styles.container}>
-      <ClientToastHandler />
       <h1 className={styles.title}>Login</h1>
       <form className={styles.form}>
         <input className={styles.input} placeholder="Email" />
@@ -25,6 +64,4 @@ const LoginPage = () => {
       </form>
     </div>
   );
-};
-
-export default LoginPage;
+}
