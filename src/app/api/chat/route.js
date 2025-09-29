07a74a5fdd51b17a7bb2ca7405fd9cf8f5ffe4ea
Iryna Zolkina <Zolkina.iryna@gmail.com>
2025-09-29 11:06:24 +0300

    commit 35

--- src/app/api/chat/route.js
+++ src/app/api/chat/route.js
@@ -1,32 +1,72 @@
+import { verifyAccessToken } from "@/lib/jwt";
+import { errorResponse } from "@/utils/apiFetch";
 import { NextRequest, NextResponse } from "next/server";
 import OpenAI from "openai";
 
 export async function POST(request) {
   const params = await request.json();
   const apiKey = process.env.DEEPSEEK_API_KEY;
+
+  const token = request.cookies.get("accessToken")?.value;
+
+  const csrfHeader = request.headers.get("x-csrf-token");
+  const csrfCookie = request.cookies.get("csrfToken")?.value;
+
+  // CSRF check
+  if (!csrfHeader || csrfHeader !== csrfCookie)
+    return errorResponse("CSRF_MISMATCH", "CSRF mismatch. Not authorized", 403);
+
+  if (!token) return errorResponse("NO_TOKEN", "No token. Not authorized", 401);
   console.log("params.prompt---", params.prompt);
   console.log("params.content---", params.content);
-  const openai = new OpenAI({
-    baseURL: "https://openrouter.ai/api/v1",
-    apiKey: apiKey,
-  });
 
-  const completion = await openai.chat.completions.create({
-    model: "deepseek/deepseek-r1-0528-qwen3-8b:free",
-    messages: [
-      { role: "system", content: params.prompt },
-      { role: "user", content: params.content },
-    ],
+  try {
+    const payload = verifyAccessToken(token);
+    console.log("Access token payload:-------------------- ", payload);
+    if (!payload) {
+      return errorResponse(
+        "INVALID_TOKEN",
+        "Access Token Invalid token. Not authorized",
+        401
+      );
+    }
 
-    temperature: 0,
-    max_tokens: 4096,
-    // max_tokens: 1024, //4096,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
-  });
+    const openai = new OpenAI({
+      baseURL: "https://openrouter.ai/api/v1",
+      apiKey: apiKey,
+    });
 
-  const response = completion.choices;
+    const completion = await openai.chat.completions.create({
+      model: "deepseek/deepseek-r1-0528-qwen3-8b:free",
+      messages: [
+        { role: "system", content: params.prompt },
+        { role: "user", content: params.content },
+      ],
+
+      temperature: 0,
+      max_tokens: 4096,
+      // max_tokens: 1024, //4096,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    });
+
+    const response = completion.choices;
+    // return NextResponse.json(
+    //   { success: true, data: { user } },
+    //   { status: 200 }
+    // );
+    console.log("response-----", response);
+    return NextResponse.json(response);
+  } catch (err) {
+    console.log("err-----", err);
+    return errorResponse(
+      "INVALID_TOKEN",
+      "Invalid token. Token verification failed",
+      401
+    );
+  }
+
   // const response = completion.choices[0].message;
   // const response = completion.choices[0].message.content;
   // console.log("******s", response[0].message.content);
@@ -50,9 +90,6 @@ export async function POST(request) {
   //     presence_penalty: 0,
   //   }),
   // });
-
-  console.log("response-----", response);
-  return NextResponse.json(response);
 }
 // import { NextRequest, NextResponse } from "next/server";
 // import OpenAI from "openai";
