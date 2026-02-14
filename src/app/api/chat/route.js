169a48885b29ed00e3bbdf2122eaca52b53acea2
Iryna Zolkina <Zolkina.iryna@gmail.com>
2026-02-14 13:25:23 +0200

    commit 45

--- src/app/api/chat/route.js
+++ src/app/api/chat/route.js
@@ -180,11 +180,11 @@ export async function POST(request) {
           { role: "user", content: content },
         ],
 
-        temperature: 0,
-        max_tokens: 256, //1024, //4096,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,
+        // temperature: 0,
+        // max_tokens: 256, //1024, //4096,
+        // top_p: 1,
+        // frequency_penalty: 0,
+        // presence_penalty: 0,
       });
 
       // const response = completion.choices;
